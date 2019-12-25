e34906a8ab2c2e587df94f5ffbb40262ac85c5c2
木瓜丸 <boke0@eugensite.com>
2019-12-26 00:45:41 +0900

    [fix] スペルミスを修正

--- src/Stream.php
+++ src/Stream.php
@@ -56,7 +56,7 @@ class Stream implements StreamInterface{
         return fread($this->fp,$length);
     }
     public function getContents(){
-        $this->rewrind();
+        $this->rewind();
         $data=stream_get_contents($this->fp);
         if($this->isSeekable()) $this->rewind();
         return $data;
